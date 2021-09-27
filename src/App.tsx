6ce2111e5c5ec00a56b52e270094f3f8364bf541
Zakaria EL KHAYDER <zelkhayder@gmail.com>
2021-09-27 17:18:11 +0100

    :bug: Fixed Vie height issue on mobile browsers

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,22 @@ import ChatsPage from "./pages/Chats";
 import ConversationPage from "./pages/Conversation";
 
 const App: React.FC = () => {
+   React.useEffect(() => {
+      const appHeight = () =>
+         document.documentElement.style.setProperty(
+            "--view-height",
+            `${window.innerHeight}px`
+         );
+
+      window.addEventListener("resize", appHeight);
+      appHeight();
+   }, []);
+
    return (
       <>
-         <Nav />
          <ChatsPage />
-         <ConversationPage />
+         <Nav />
+         {/* <ConversationPage /> */}
       </>
    );
 };
